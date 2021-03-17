2a2c63fb16077ffa10b4ad1672ae3c4c54bd182e
Zio0708 <jiho9478@naver.com>
2021-03-17 23:30:29 +0900

    study0317.과제

---
+++ src/backjoon031603.java
@@ -0,0 +1,9 @@
+public class backjoon031603 {
+    public static void main(String[] args){
+
+    }
+    public static void solution(int n, int[] number){
+        //인턴 과제 진행중..
+        return;
+    }
+}
